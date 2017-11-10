libmpack/libmpack-lua

commit f3c8bf63c189a2ab114c099b5cc24e8083dc4f6a

Allow linking against system libmpack
Also fix compilation with USE_SYSTEM_LUA=1 introduced by #4

Makefile
@@ -2,6 +2,13 @@
 
 # distributors probably want to set this to 'yes' for both make and make install
 USE_SYSTEM_LUA ?= no
+USE_SYSTEM_MPACK ?= no
+ifneq ($(USE_SYSTEM_MPACK),no)
+# Can't use luarocks to build if linking against system libmpack because
+# apparently luarocks doesn't let you specify extra linker flags from the
+# command line
+USE_SYSTEM_LUA := 1
+endif
 
 # Lua-related configuration
 MPACK_LUA_VERSION ?= 5.1.5
@@ -45,8 +52,16 @@ LUA_LIB := $(shell $(PKG_CONFIG) --libs lua-$(MPACK_LUA_VERSION_NOPATCH) 2>/dev/
 INCLUDES = $(LUA_INCLUDE)
 LIBS = $(LUA_LIB)
 
-LUA_CMOD_INSTALLDIR := $(shell $(PKG_CONFIG) --variable=INSTALL_CMOD lua-$(MPACK_LUA_VERSION_NOPATCH) 2>/dev/null || echo "/usr/lib/lua/$(MPACK_LUA_VERSION_NOPATCH)")
+ifeq ($(USE_SYSTEM_MPACK),no)
+CFLAGS += -DMPACK_USE_AMALGAMATION
+MPACK_SRC = mpack-src
+else
+MPACK_SRC =
+LIBS += $(shell $(PKG_CONFIG) --libs mpack 2>/dev/null || echo "-lmpack")
+CFLAGS += $(shell $(PKG_CONFIG) --cflags mpack 2> /dev/null)
+endif
 
+LUA_CMOD_INSTALLDIR := $(shell $(PKG_CONFIG) --variable=INSTALL_CMOD lua-$(MPACK_LUA_VERSION_NOPATCH) 2>/dev/null || echo "/usr/lib/lua/$(MPACK_LUA_VERSION_NOPATCH)")
 
 # Misc
 # Options used by the 'valgrind' target, which runs the tests under valgrind
@@ -95,11 +110,11 @@ gdb: $(BUSTED) $(MPACK)
 		$(DEPS_PREFIX)/lib/luarocks/rocks/busted/2.0.rc12-1/bin/busted test.lua
 
 ifeq ($(USE_SYSTEM_LUA),no)
-$(MPACK): $(LUAROCKS) mpack-src
-	$(LUAROCKS) make CFLAGS='$(CFLAGS)'
+$(MPACK): $(LUAROCKS) $(MPACK_SRC) lmpack.c
+	$(LUAROCKS) make CFLAGS='$(CFLAGS)' $(LUAROCKS_LDFLAGS)
 else
-$(MPACK): mpack-src
-	$(CC) -shared $(CFLAGS) $(INCLUDES) $(LDFLAGS)-o $@ $(LIBS)
+$(MPACK): lmpack.c $(MPACK_SRC)
+	$(CC) -shared $(CFLAGS) $(INCLUDES) $(LDFLAGS) $< -o $@ $(LIBS)
 endif
 
 $(BUSTED): $(LUAROCKS)

lmpack.c
@@ -16,13 +16,18 @@
  */
 #define LUA_LIB
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
 #include <lua.h>
 #include <lauxlib.h>
 
-#define MPACK_API static
-#include "mpack-src/src/mpack.c"
+#ifdef MPACK_USE_AMALGAMATION
+# define MPACK_API static
+# include "mpack-src/src/mpack.c"
+#else
+# include <mpack.h>
+#endif
 
 #define UNPACKER_META_NAME "mpack.Unpacker"
 #define PACKER_META_NAME "mpack.Packer"

.travis.yml
@@ -13,6 +13,27 @@ addons:
   apt:
     packages:
       - valgrind
+      - liblua5.1-0-dev
+
+matrix:
+  include:
+    - os: linux
+      env: USE_SYSTEM_LUA=1
+      script:
+        - make ci-test
+        - ldd -d mpack.so | grep -q /usr/lib/x86_64-linux-gnu/liblua
+
+    - os: linux
+      env: USE_SYSTEM_LUA=1 USE_SYSTEM_MPACK=1
+      script:
+        - export PREFIX=$(pwd)/mpack-src/usr
+        - export LD_LIBRARY_PATH=${PREFIX}/lib
+        - export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+        - make mpack-src
+        - make -C mpack-src config=release install
+        - make ci-test
+        - ldd -d mpack.so | grep -q ${PREFIX}/lib/libmpack
+        - ldd -d mpack.so | grep -q /usr/lib/x86_64-linux-gnu/liblua
 
 script:
   make ci-test

commit 3eda97c0ac4108c0030b70e4a4ea186542f67025

Fix building with luarocks
Building with `luarocks make` was broken in f3c8bf63 since
DMPACK_USE_AMALGAMATION is not defined by default. Change defines in
lmpack.c to only use system libmpack if DMPACK_USE_SYSTEM is defined.

Makefile
@@ -54,12 +54,11 @@ INCLUDES = $(LUA_INCLUDE)
 LIBS =
 
 ifeq ($(USE_SYSTEM_MPACK),no)
-CFLAGS += -DMPACK_USE_AMALGAMATION
 MPACK_SRC = mpack-src
 else
 MPACK_SRC =
 LIBS += $(shell $(PKG_CONFIG) --libs mpack 2>/dev/null || echo "-lmpack")
-CFLAGS += $(shell $(PKG_CONFIG) --cflags mpack 2> /dev/null)
+CFLAGS += -DMPACK_USE_SYSTEM $(shell $(PKG_CONFIG) --cflags mpack 2> /dev/null)
 endif
 
 LUA_CMOD_INSTALLDIR ?= $(shell $(PKG_CONFIG) --variable=INSTALL_CMOD $(LUA_IMPL) 2>/dev/null || echo "/usr/lib/lua/$(MPACK_LUA_VERSION_NOPATCH)")

lmpack.c
@@ -23,11 +23,11 @@
 #include <lua.h>
 #include <luaconf.h>
 
-#ifdef MPACK_USE_AMALGAMATION
+#ifdef MPACK_USE_SYSTEM
+# include <mpack.h>
+#else
 # define MPACK_API static
 # include "mpack-src/src/mpack.c"
-#else
-# include <mpack.h>
 #endif
 
 #define UNPACKER_META_NAME "mpack.Unpacker"